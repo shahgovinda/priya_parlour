d101488b6fd77730066b0db101f60b34f48b3c6e
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-11-10 17:54:45 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,12 @@
 
 export const Constant = {
-    SiteName : 'Numa.',
-    PhoneNumber: '+91 848484 999888',
-    Address: '123 Main Street, New York',
-    Email: 'priyajayprakash@gmail.com',
+    SiteName : 'priya.',
+    PhoneNumber: '+91 84520 17063',
+    Address: 'Saibaba Nagar, Borivali West, Mumbai 400092',
+    Email: 'priyajayprakash456@gmail.com',
     Timing: ' 10:00 AM - 10:00 PM',
-    Instagram: 'https://www.instagram.com/priya_parlour28?igsh=dm84OHh2NHZqZGFp',
-    Google: 'https://www.google.com',
+    Instagram: 'https://www.instagram.com/priyashah2812?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+    Google: 'https://share.google/YOPoswv5YRuZuqddV',
     Facebook:'https://www.facebook.com',
     Youtube:'https://www.youtube.com',
     Twitter:'https://twitter.com'
